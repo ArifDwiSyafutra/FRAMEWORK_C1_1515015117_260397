c5e586cb0f80ee5f0da5e423ae52b427204dab51
arifdwisyafutra <arifdwi286@gmail.com>
2017-03-24 15:54:48 +0000

    tugas 4 selesai

---
+++ app/Jadwal_Kuliah.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jadwal_Kuliah extends Model
+{
+     protected $table = 'Jadwal_Kuliah';
+    //protected $fillable = ['mahasiswa_id','ruangan_id','dosen_matakuliah_id'];
+}
